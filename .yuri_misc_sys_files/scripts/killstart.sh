aed01c571e22bf4424336d7df16616b7e0a51bd3
yuri-rage <yuri.rage@gmail.com>
2014-06-08 15:31:33 +0300

    scripted update: Sun, 08 Jun 2014  15:31:33

---
+++ .yuri_misc_sys_files/scripts/killstart.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# start process(es) after killing all instances of the same
+
+# yuri - Jun 2014
+
+
+# if no argument, exit with error status
+usage(){
+	echo "Usage: $0 command1 command2 .."
+    echo -e "\nCommands with arguments must be quoted:"
+    echo "       $0 'command --with args'"
+	exit 1
+}
+[ -z "$1" ] && usage
+
+# treat each argument as a separate command
+
+# kill all instances of each command
+for ARG in "$@"; do
+  # accept commands with multiple arguments
+  BASECMD=`echo "$ARG" | awk '{ print $1 }'`
+  # if process(es) running, kill 'em
+  pgrep $BASECMD && pkill $BASECMD
+done
+
+# run specified command(s)
+for ARG in "$@"; do
+  $ARG &
+done
